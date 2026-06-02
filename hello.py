d919532651286ac7b9b9938db42b695c4480984e
mrayusha8-hue <mrayusha8@gmail.com>
2026-06-02 17:56:36 +0545

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,17 @@
+#Identation
+if 1==1:
+    print('successful')
+'''if 1==1:
+print('successful')'''
+
+#printing multiple lines in one using 'end'parameter
+print('hello', end='')
+print('world')
+
+#variables
+x=60
+y="challenge"
+print(x,y)
+
+print(x+y)
+
