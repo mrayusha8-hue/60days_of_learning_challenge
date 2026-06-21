f2f7571c13f8e13e70e2c77b04300dc44ee56b82
mrayusha8-hue <mrayusha8@gmail.com>
2026-06-21 16:19:47 +0545

    explored ones,zeros,copy,view,shape,reshape,,random generator in numpy

--- Python/numpy_intro.py
+++ Python/numpy_intro.py
@@ -63,3 +63,46 @@ print(a.dtype)
 arr = np.array([-1, 0, 1])
 a = arr.astype(bool)
 print(a)
+
+
+#ones(),zeros()
+print(np.ones(2))
+print(np.zeros(3))
+
+#Copy and View
+a = np.array([1,2,3,4,5])
+x = a.copy()   # Copy is a new array
+a[1] = 86
+print(a)
+print(x)
+
+a = np.array([1,2,3,4,5])
+x = a.view()   # View is just view of original array
+a[1] = 86
+print(a)
+print(x)
+
+#Shape and Reshape
+a = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+print(a.shape)
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,])
+newarr = arr.reshape(6, 2)
+print(newarr)
+
+a = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+print(a.reshape(-1))  #2D to 1D
+
+#random generator
+from numpy import random
+x = random.rand()   #for float
+print(x)
+
+x = random.randint(5)  #for int
+print(x)
+
+x=random.randint(100, size=(5)) #for array
+print(x)
+
+x=random.randint(100, size=(2,3))
+print(x)
