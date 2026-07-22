7e4adbc3debcc998e5c8d23ac99d7a88e8e43829
mrayusha8-hue <mrayusha8@gmail.com>
2026-07-22 20:40:35 +0545

    Refine Streamlit dashboard layout and organize sections

--- expense_traker/notebook/app.py
+++ expense_traker/notebook/app.py
@@ -1,3 +1,4 @@
+# Import libraries
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,15 +10,50 @@ st.set_page_config(
     layout = "wide"
 )
 
-# Title
-st.title("Personal_Expense_Tracker")
+# Dashboard Title
+st.title("💰 Personal Expense Tracker")
 
-#Project Overview
+# Project Overview
 st.divider()
 st.subheader("Project Overview")
-st.write("This dashboard helps analyze expenses through interactive visualizations.")
+st.markdown("This dashboard helps analyze expenses through interactive visualizations.")
 
 
-
+# Load Data
 df = pd.read_csv("cleaned_expense.csv")
-st.write(df)
+st.success("Data loaded successfully")
+
+st.divider()
+
+# Dashboard Summary
+st.header("📊  Dashboard Summary")
+st.write("Key performance Indicators(KPIs) will be be displayed here")
+st.divider()
+
+st.caption("Personal Expense Tracker | Built using Python, Pandas and Streamlit")
+st.divider()
+
+#Monthly Expense Section
+st.header("📈 Monthly Expense Trend")
+st.info(" Monthly expense trend chart will be displayed here.")
+
+st.divider()
+
+#Categort wise Expense
+st.header("📂 Category-wise Expenses")
+st.info(" This section will display category-wise spending analysis.")
+
+st.divider()
+
+# Pie Chart Section
+st.header("🥧 Expense Distribution")
+st.info(" Expense distribution by category will be displayed here.")
+
+#Dataset Preview
+st.divider()
+st.header("📄 Dataset Preview")
+st.dataframe(df.head())
+
+# Footer
+st.caption("Personal Expense Tracker | Built using Python, Pandas and Streamlit")
+st.divider()
